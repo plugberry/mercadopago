289ed5565d20568a701500266ce31a8538523804
raguero </SUlem@2017/>
2020-10-08 10:14:30 -0300

    Revert "[IMP] Manage payment method"
    This reverts commit 59b25bb4
    
    Wrong work branch.

--- payment_mercadopago/models/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import payment
-from . import inherit_payment_token
