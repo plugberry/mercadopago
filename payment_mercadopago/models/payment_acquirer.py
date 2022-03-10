ed43dc16b908a12b9f3458201fade2254935c83f
Bruno Zanotti <bz@adhoc.com.ar>
2022-03-10 17:20:10 -0300

    [IMP] payment_mercadopago: general item

--- payment_mercadopago/models/payment_acquirer.py
+++ payment_mercadopago/models/payment_acquirer.py
@@ -20,6 +20,39 @@ class PaymentAcquirer(models.Model):
     mercadopago_access_token = fields.Char('MercadoPago Access Token', required_if_provider='mercadopago')
     is_validation = fields.Boolean()
 
+    # MercadoPago general item fields
+    mercadopago_item_id = fields.Char('Item ID')
+    mercadopago_item_title = fields.Char('Item Title')
+    mercadopago_item_description = fields.Char('Item Description')
+    mercadopago_item_category = fields.Selection(
+        string='MercadoPago Category', help="The category",
+        selection=[
+            ('art', "Collectibles & Art"),
+            ('baby', "Toys for Baby, Stroller, Stroller Accessories, Car Safety Seats"),
+            ('coupons', "Coupons"),
+            ('donations', "Donations"),
+            ('computing', "Computers & Tablets"),
+            ('cameras', "Cameras & Photography"),
+            ('video_games', "Video Games & Consoles"),
+            ('television', "LCD, LED, Smart TV, Plasmas, TVs"),
+            ('car_electronics', "Car Audio, Car Alarm Systems & Security, Car DVRs, Car Video Players, Car PC"),
+            ('electronics', "Audio & Surveillance, Video & GPS, Others"),
+            ('automotive', "Parts & Accessories"),
+            ('entertainment', "Music, Movies & Series, Books, Magazines & Comics, Board Games & Toys"),
+            ('fashion', "Men's, Women's, Kids & baby, Handbags & Accessories, Health & Beauty, Shoes, Jewelry & Watches"),
+            ('games', "Online Games & Credits"),
+            ('home', "Home appliances. Home & Garden"),
+            ('musical', "Instruments & Gear"),
+            ('phones', "Cell Phones & Accessories"),
+            ('services', "General services"),
+            ('learnings', "Trainings, Conferences, Workshops"),
+            ('tickets', "Tickets for Concerts, Sports, Arts, Theater, Family, Excursions tickets, Events & more"),
+            ('travels', "Plane tickets, Hotel vouchers, Travel vouchers"),
+            ('virtual_goods', "E-books, Music Files, Software, Digital Images, PDF Files and any item which can be electronically stored in a file, Mobile Recharge, DTH Recharge and any Online Recharge"),
+            ('others', "Other categories"),
+        ],
+    )
+
     @api.model
     def _get_compatible_acquirers(self, *args, currency_id=None, **kwargs):
         """ Override of payment to unlist MercadoPago acquirers when the currency is not ARS. """
