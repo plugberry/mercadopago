c193a39b04883f4ffd09bbb81d70b10f0abc7a81
Bruno Zanotti <bz@adhoc.com.ar>
2022-07-21 16:43:29 -0300

    [FIX] payment_mercadopago: remove MercadoPago item fields
    [WIP0] backport 15 module changes.
    
    [FIX] Reordeno para poder comparar con el anterior
    
    [FIX] CLEAN controller code
    
    [FIX] Modifico el TMP para que CVV sea tipo pass
    [FIX] Agrego customer_id en el token para no buscarlo todo el tiempo
    [FIX] Agrego mas datos de customer para mejorar la validacion sin CVV
    
    Create push_to_another_repo.yml
    
    [DEL] remove unused gh action

--- payment_mercadopago/models/mercadopago_request.py
+++ payment_mercadopago/models/mercadopago_request.py
@@ -5,6 +5,10 @@ from odoo import _
 from odoo.exceptions import UserError
 from werkzeug import urls
 from babel.dates import format_datetime
+import requests
+
+MP_URL = "https://api.mercadopago.com/"
+
 
 _logger = logging.getLogger(__name__)
 
@@ -23,6 +27,7 @@ class MercadoPagoAPI():
         request_options = RequestOptions(acquirer.mercadopago_access_token, platform_id="BVH38T5N7QOK0PPDGC2G")
         self.mp = mercadopago.SDK(acquirer.mercadopago_access_token, request_options=request_options)
         self.sandbox = not acquirer.state == "enabled"
+        self.mercadopago_access_token = acquirer.mercadopago_access_token
 
     def check_response(self, resp):
         if resp['status'] in [200, 201]:
@@ -43,6 +48,44 @@ class MercadoPagoAPI():
                 'err_msg': "Server Error"
             }
 
+    def check_api_response(self, resp):
+        resp_json = resp.json()
+        if resp.ok:
+            return resp_json
+        elif resp_json.get('cause'):
+            return {
+                'err_code': resp_json['cause'][0].get('code'),
+                'err_msg': resp_json['cause'][0].get('description')
+            }
+        elif resp_json.get('error'):
+            return {
+                'err_code': resp_json.get('status', 0),
+                'err_msg': resp_json.get('error')
+            }
+        else:
+            return {
+                'err_code': 500,
+                'err_msg': "Server Error"
+            }
+
+    def unlink_card_token(self, customer_id, card_id):
+        api_url = MP_URL + "v1/customers/%s/cards/%s" % (customer_id, card_id)
+        headers = {"Authorization": "Bearer %s" % self.mercadopago_access_token}
+        response = requests.delete(api_url, headers=headers)
+
+    #create Test User
+    def create_test_user(self):
+        api_url = MP_URL + "users/test_user" 
+        headers = {"Authorization": "Bearer %s" % self.mercadopago_access_token}
+        request_data = {"site_id": "MLA"}
+        response = requests.post(api_url, headers=headers, json=request_data)
+        resp = self.check_api_response(response)
+
+        if resp.get('err_code'):
+            raise UserError(_("MercadoPago Error:\nCode: %s\nMessage: %s" % (resp.get('err_code'), resp.get('err_msg'))))
+        else:
+            return resp
+
     # Preference
     def create_preference(self, preference):
         resp = self.mp.preference().create(preference)
@@ -60,7 +103,6 @@ class MercadoPagoAPI():
         values = {'email': email}
         resp = self.mp.customer().search(values)
         resp = self.check_response(resp)
-
         if resp.get('err_code'):
             raise UserError(_("MercadoPago Error:\nCode: %s\nMessage: %s" % (resp.get('err_code'), resp.get('err_msg'))))
         else:
@@ -74,7 +116,6 @@ class MercadoPagoAPI():
         values = {'email': email}
         resp = self.mp.customer().create(values)
         resp = self.check_response(resp)
-
         if resp.get('err_code'):
             raise UserError(_("MercadoPago Error:\nCode: %s\nMessage: %s" % (resp.get('err_code'), resp.get('err_msg'))))
         else:
@@ -118,6 +159,8 @@ class MercadoPagoAPI():
         """
         MercadoPago payment
         """
+        capture, validation_capture_method = self.validation_capture_method(tx)
+
         values = {
             "token": cvv_token or self.get_card_token(tx.payment_token_id.token),
             "installments": tx.payment_token_id.installments,
@@ -125,16 +168,18 @@ class MercadoPagoAPI():
             "description": "Odoo ~ MercadoPago payment",
             "payment_method_id": tx.payment_token_id.acquirer_ref,
             "binary_mode": True,
+            "external_reference": tx.reference,
             "payer": {
-                # Pasamos el main del token porque es el mail con el que se tokenizó la tarjeta
-                # (podría no coincidir con el partner de la tx)
+                "type": "customer",
+                "id": tx.payment_token_id.customer_id if tx.payment_token_id and tx.payment_token_id.customer_id else None,
                 "email": tx.payment_token_id.partner_id.email,
+                "first_name": tx.partner_name,
             },
             "additional_info": {
                 "items": [{
-                    "id": 'Venta de ecommerce',
-                    "title": 'Venta de ecommerce',
-                    "description": 'Venta de ecommerce',
+                    "id": '001',
+                    "title": _('Venta de ecommerce'),
+                    "description": _('Venta de ecommerce'),
                     "category_id": 'others',
                     "quantity": 1,
                     "unit_price": amount,
@@ -151,24 +196,32 @@ class MercadoPagoAPI():
                     "registration_date": format_datetime(tx.partner_id.create_date),
                 },
             },
-            "notification_url": urls.url_join(tx.acquirer_id.get_base_url(), '/payment/mercadopago/notify?source_news=webhooks'),
+            "notification_url": urls.url_join(tx.acquirer_id.get_base_url(), '/payment/mercadopago/notify/%s?source_news=webhooks' % tx.acquirer_id.id),
             "capture": capture
         }
+
         if tx.payment_token_id.issuer:
             values.update(issuer_id=tx.payment_token_id.issuer)
 
-        # TODO: revisar si deberíamos hacer esto directamente para todos los casos y ver si guardamos el dato antes
-        if cvv_token or capture:
-            customer_id = self.get_customer_profile(tx.payment_token_id.partner_id.email)
-            values.update({"payer": {"type": 'customer', 'id': customer_id}})
+        if hasattr(tx.partner_id, 'l10n_latam_identification_type_id'):
+            values['payer']['identification'] = {
+                "number": tx.partner_id.vat,
+                "type": tx.partner_id.l10n_latam_identification_type_id.name,
+            }
+
+        if self.sandbox:
+            _logger.info('values:\n%s' % values)
 
         resp = self.mp.payment().create(values)
-        if self.sandbox:
-            _logger.info('Payment Result:\n%s' % resp)
+
+        _logger.info('Payment Result:\n%s' % resp)
         resp = self.check_response(resp)
         if resp.get('err_code'):
             raise UserError(_("MercadoPago Error:\nCode: %s\nMessage: %s" % (resp.get('err_code'), resp.get('err_msg'))))
         else:
+            if validation_capture_method == 'refund_payment' and resp['status'] in ['approved'] :
+                _logger.info(_('Refund validation payment id: %s ' % resp['id']))
+                self.payment_refund(resp['id'])
             return resp
 
     def payment_cancel(self, payment_id):
@@ -194,3 +247,51 @@ class MercadoPagoAPI():
             raise UserError(_("MercadoPago Error:\nCode: %s\nMessage: %s" % (resp.get('err_code'), resp.get('err_msg'))))
         else:
             return resp
+
+    def payment_refund(self, payment_id, amount=0):
+        """
+        MercadoPago refund payment
+        """
+        if amount:
+            values = {
+                "amount": amount
+            }
+            resp = self.mp.refund().create(payment_id, values)
+        else:
+            resp = self.mp.refund().create(payment_id)
+
+        resp = self.check_response(resp)
+        if resp.get('err_code'):
+            raise UserError(_("MercadoPago Error:\nCode: %s\nMessage: %s" % (resp.get('err_code'), resp.get('err_msg'))))
+        else:
+            return resp
+
+    def payment_can_deferred_capture(self, payment_method_id):
+
+        resp = self.mp.payment_methods().list_all()
+        resp = self.check_response(resp)
+        if type(resp) is dict and resp.get('err_code'):
+            _logger.error(_("MercadoPago Error:\nCode: %s\nMessage: %s" % (resp.get('err_code'), resp.get('err_msg'))))
+            return False
+        payment = [d for d in resp if d['id'] == payment_method_id and d['status' ]=='active']
+        if len(payment):
+            return payment[0]['deferred_capture'] == 'supported'
+        return False
+
+    def validation_capture_method(self, tx):
+        """
+        Validation capture method
+            If transaction type is a validation,
+            Return a strategy to no capture the payment or  refund it after validation.
+            Return two values
+             - Capture: if the transaction should be captured
+             - Method: If a refund should be made.
+        """
+        if tx.type != 'validation':
+            return True, None
+
+        payment_method_id = tx.payment_token_id.acquirer_ref
+        if self.payment_can_deferred_capture(payment_method_id):
+            return False, 'deferred_capture'
+        else:
+            return True, 'refund_payment'
