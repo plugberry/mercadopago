22c461aba7dc8d53cdf3b23feaa0abcbe04a35b9
filoquin <filoquin@gmail.com>
2023-03-13 22:36:27 +0000

    [IMP] add wizards check payment in MP
    closes plugberry/mercadopago#55
    
    Signed-off-by: Bruno Zanotti <bz@adhoc.com.ar>

--- payment_mercadopago/__init__.py
+++ payment_mercadopago/__init__.py
@@ -4,6 +4,7 @@
 ##############################################################################
 from . import models
 from . import controllers
+from . import wizards
 from odoo.addons.payment.models.payment_acquirer import create_missing_journal_for_acquirers
 from odoo.addons.payment import reset_payment_provider
 
