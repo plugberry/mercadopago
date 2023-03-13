2615b1cc6ec13e25b78c81bea025e6971269ff23
filoquin <filoquin@gmail.com>
2023-03-13 22:36:27 +0000

    [IMP] add wizards check payment in MP
    X-original-commit: 22c461aba7dc8d53cdf3b23feaa0abcbe04a35b9

--- payment_mercadopago/__manifest__.py
+++ payment_mercadopago/__manifest__.py
@@ -3,9 +3,19 @@
     'name': 'MercadoPago Payment Acquirer',
     'category': 'Accounting/Payment Acquirers',
     'summary': 'Payment Acquirer: MercadoPago',
+<<<<<<< HEAD
     'version': "15.0.1.3.3",
     'author': 'ADHOC SA - Axcelere S.A.S',
     'website': 'www.adhoc.com.ar, www.axcelere.com',
+||||||| parent of bbe24ce (temp)
+    'version': '13.0.1.11.0',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+=======
+    'version': '13.0.1.15.0',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+>>>>>>> bbe24ce (temp)
     'description': """MercadoPago Payment Acquirer""",
     'depends': ['payment'],
     'external_dependencies': {
@@ -13,7 +23,16 @@
     },
     'data': [
         'views/payment_views.xml',
+<<<<<<< HEAD
         'views/payment_mercadopago_templates.xml',
+||||||| parent of bbe24ce (temp)
+        'views/payment_transaction_views.xml',
+        'views/assets.xml',
+=======
+        'views/payment_transaction_views.xml',
+        'views/assets.xml',
+        'wizards/check_payments.xml',
+>>>>>>> bbe24ce (temp)
         'data/payment_acquirer_data.xml',
     ],
     'demo': [
