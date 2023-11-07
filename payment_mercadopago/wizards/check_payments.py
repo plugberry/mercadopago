70783906eb61703461f489511881055cef53ad68
Gabriela Rivero <regaby@gmail.com>
2023-11-07 08:32:52 -0300

    [FIX] cant validate_tree 'payment.mercadopago.check_payment.line' object has no attribute '_mercadopago_s2s_validate_tree'

--- payment_mercadopago/wizards/check_payments.py
+++ payment_mercadopago/wizards/check_payments.py
@@ -48,7 +48,7 @@ class PaymentMercadopagoCheckPayment(models.TransientModel):
             if 'results' in payments['response']:
                 for payment in payments['response']['results']:
                     if not self.confirmed or payment.get('status') in ['refunded','approved','authorized','charged_back']:
-                        transaction_id = tx_ids.filtered(lambda t: t.reference == payment['external_reference'])                    
+                        transaction_id = tx_ids.filtered(lambda t: t.reference == payment['external_reference'])
                         if transaction_id and self.no_match and self.check_status(payment.get('status'), transaction_id.state):
                             continue
                         lines.append((0, 0, {
@@ -99,8 +99,8 @@ class PaymentMercadopagoCheckPaymentLine(models.TransientModel):
                 txt += ['%s: %s' % (x, payment[x]) for x in payment]
                 txt += ['---------------------------']
                 try:
-                    rec._mercadopago_s2s_validate_tree(payment)
-                except:
-                    _logger.error('cant validate_tree')
+                    rec.transaction_id._mercadopago_s2s_validate_tree(payment)
+                except Exception as e:
+                    _logger.error('cant validate_tree %s' % e)
 
         raise UserError("%s" % ' \n'.join(txt))
